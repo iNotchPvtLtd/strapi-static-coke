8081fb6206976e7904f23081b99ced73152e35b3
saravana goutham <smurugesan@inotchglobal.com>
2025-05-16 17:24:06 +0530

    coke data

---
+++ out/_next/static/sEf9jCVQNnVLTaYVly4Fo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/557-114d74155ee9aad6.js","static/css/09910b0debd6862c.css","static/chunks/pages/index-f826178e501d10aa.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
